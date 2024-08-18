01e01184ceeef2b4a5fced799eba7b3aa59fcd55
Kim Tran <lucian457fcde@gmail.com>
2024-08-18 11:14:35 +0700

    origin_

--- resources/views/components/new_full_map.blade.php
+++ resources/views/components/new_full_map.blade.php
@@ -24,8 +24,8 @@
 
 
     <div class="iframeBorder">
-        <iframe src="http://localhost:5173/"
-            style="position: absolute; top: 10%; left:10%; width: 80%; height: 90%; border: 0;" allowfullscreen
+        <iframe src="http://localhost:5174/"
+            style="position: absolute; top: 0%; left:0%; width: 100%; height: 100%; border: 0;" allowfullscreen
             loading="lazy" referrerpolicy="no-referrer-when-downgrade">
         </iframe>
     </div>
