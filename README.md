49c50382a461cb8cadfb82b2e840c0453a851905
vu0cay <vhn016260@gmail.com>
2024-08-21 08:52:42 +0700

    ok

---
+++ README.md
@@ -0,0 +1,3 @@
+-Chạy map openlayer trước, sau đó lấy link paste vào iframe của file new_full_map.blade.php trong resources/view/components 
+
+-Có thể gặp lỗi manifest not found khi chạy php artisan serve, chạy lệnh npm run dev sau đó chạy php artisan server lại rồi vào link mới
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,32 @@
+<x-app-layout>
+{{-- 
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot> --}}
+
+    <div class="flex flex-col">
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">
+                        {{ __("You're logged in!") }}
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="iframeBorder">
+            {{-- <iframe src="http://localhost:5174/"
+                style="position: absolute; width: 50%; height: 50%; border: 0;" allowfullscreen
+                loading="lazy" referrerpolicy="no-referrer-when-downgrade">
+            </iframe> --}}
+            <iframe
+                src="http://localhost:5174/"
+                width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+    </div>
+
+</x-app-layout>
